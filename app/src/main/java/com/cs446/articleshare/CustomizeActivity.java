be95a517806c1a021828be382090885b8dd2d2e4
Andy Baek <andybaek95@gmail.com>
2018-02-15 16:52:30 -0500

    two activities, no actionbar

--- app/src/main/java/com/cs446/articleshare/CustomizeActivity.java
+++ app/src/main/java/com/cs446/articleshare/CustomizeActivity.java
@@ -1,20 +1,20 @@
 package com.cs446.articleshare;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class CustomizeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_customize);
     }
 
-    public void nextActivity(View view) {
-        Intent intent = new Intent(this, CustomizeActivity.class);
+    public void previousActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
