32d69768f4a965dbcedaff702d849891ae41f6a5
Andy Baek <andybaek95@gmail.com>
2018-02-25 18:37:22 -0500

    ShareActivity creates a Toast

--- app/src/main/java/com/cs446/articleshare/ShareActivity.java
+++ app/src/main/java/com/cs446/articleshare/ShareActivity.java
@@ -1,20 +1,25 @@
 package com.cs446.articleshare;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ShareActivity extends AppCompatActivity {
+
+    private EditText tweet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+        tweet = (EditText) findViewById(R.id.tweet);
     }
 
-    public void previousActivity(View view) {
-        Intent intent = new Intent(this, InputActivity.class);
-        startActivity(intent);
+    public void postTweet(View view) {
+        Toast.makeText(this, tweet.getText(), Toast.LENGTH_LONG).show();
     }
+
 }
